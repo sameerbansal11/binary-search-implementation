9568f595460ea7dcbe3c5e2afaea3f45c6d28566
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:29:53 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# binary-search-implementation
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("Binary Search Algorithm Implementation in Python")
+    print("Project: binary-search-implementation")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
